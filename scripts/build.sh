fec96968a3ec82f874ef59cd6788ca68ceae6712
William Gulian <williamgulian@gmail.com>
2018-05-22 01:43:28 -0400

    Replace CI with that from smartpass-homepage

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env sh
 set -e
 
-apk add --no-cache bash python tree nodejs-npm
+if [[ -n "$CI_REGISTRY_USER" ]]; then
+  echo "Logging to GitLab Container Registry with CI credentials..."
+  docker login -u "$CI_REGISTRY_USER" -p "$CI_REGISTRY_PASSWORD" "$CI_REGISTRY"
+  echo ""
+fi
+
 
 GCS_TAG_NAME=${CI_COMMIT_REF_NAME}
-export STATIC_URL="https://storage.googleapis.com/courier-static/$GCS_TAG_NAME/"
 
 if ! scripts/compile_frontend.sh ; then
     echo 'Failed to compile frontend, cannot continue'
@@ -14,20 +18,19 @@ fi
 tree ./dist
 echo "Static Assets: $(du -sh ./dist | awk '{print $1}')"
 
-# GCE_SERVICE_ACCOUNT_KEY is a secret in Gitlab
-if [ -n "$GCE_SERVICE_ACCOUNT_KEY" ]; then
-    echo "$GCE_SERVICE_ACCOUNT_KEY" > gce-credential-key.json
+PROJ_BASE=$CI_REGISTRY/$CI_PROJECT_NAMESPACE/$CI_PROJECT_NAME
+echo "ref tag: $PROJ_BASE:$CI_COMMIT_REF_NAME"
+echo "sha tag: $PROJ_BASE:$CI_COMMIT_SHA"
 
-    wget -O google-cloud-sdk.tar.gz  https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-183.0.0-linux-x86_64.tar.gz
-    tar xzf google-cloud-sdk.tar.gz
+docker build \
+    -t "$PROJ_BASE:$CI_COMMIT_REF_NAME" \
+    -t "$PROJ_BASE:$CI_COMMIT_SHA" \
+    -f Dockerfile .
 
-    PATH="$PATH:$(pwd)/google-cloud-sdk/bin"
-
-    gcloud config configurations create default
-    gcloud auth activate-service-account --key-file=gce-credential-key.json
-    gcloud config set project notify-messenger
-    gsutil rsync -r ./dist gs://courier-static/${GCS_TAG_NAME}/frontend
+if [ -z "$CI_SKIP_UPLOAD" ]; then
+    docker push "$PROJ_BASE:$CI_COMMIT_REF_NAME"
+    docker push "$PROJ_BASE:$CI_COMMIT_SHA"
 else
-    echo '$GCE_SERVICE_ACCOUNT_KEY not set, skipping asset upload'
+    echo 'Skipping upload to registry'
 fi
 
