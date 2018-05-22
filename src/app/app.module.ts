120cf323344f332206d0099397f6ef1636018917
William Gulian <williamgulian@gmail.com>
2018-05-21 22:01:15 -0400

    Fix compile error in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -68,9 +68,8 @@ import { InvitationCardComponent } from './invitation-card/invitation-card.compo
 import { FuturePassCardComponent } from './future-pass-card/future-pass-card.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { GoogleLoginService } from './google-login.service';
-
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import { GradientButtonComponent } from './gradient-button/gradient-button.component';
+
 const appRoutes: Routes = [
   {path: '', redirectTo: '/main', pathMatch: 'full'},
   {path: 'main', component: MainPageComponent},
