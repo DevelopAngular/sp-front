b9a7461bcb79594a7308d77988b1d774b625f6cc
William Gulian <williamgulian@gmail.com>
2018-05-24 19:15:35 -0400

    Change consent menu to be material

--- src/app/consent-menu/consent-menu.component.ts
+++ src/app/consent-menu/consent-menu.component.ts
@@ -1,27 +1,17 @@
-import { Component, OnInit, Input, Output, EventEmitter, Inject } from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { Component, Inject } from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-consent-menu',
   templateUrl: './consent-menu.component.html',
   styleUrls: ['./consent-menu.component.css']
 })
-export class ConsentMenuComponent implements OnInit {
+export class ConsentMenuComponent {
 
   content: string;
 
-  constructor(public dialogRef: MatDialogRef<ConsentMenuComponent>, @Inject(MAT_DIALOG_DATA) public data: any[]) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any[]) {
     this.content = data['content'];
   }
 
-  ngOnInit() {
-
-  }
-
-  click(val){
-    // if(val)
-    //   this.onAccept.emit(true);
-    // else
-    //   this.onDecline.emit(false);
-  }
 }
