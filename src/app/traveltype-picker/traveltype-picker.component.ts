f451285f93a5a314aace1d6b111db82d83aaeb2e
PlanetCookieX <PlanetCookieX@gmail.com>
2018-06-11 23:53:13 -0400

    attempted travel_type picker

--- src/app/traveltype-picker/traveltype-picker.component.ts
+++ src/app/traveltype-picker/traveltype-picker.component.ts
@@ -11,9 +11,27 @@ export class TraveltypePickerComponent implements OnInit {
 
   @Output() onSelect:EventEmitter<any> = new EventEmitter();
 
+  choice1:string;
+  choice2:string;
+  selectedChoice:string;
   constructor() { }
 
   ngOnInit() {
+    this.choice1 = this.choices.length==2?'Round-trip':(this.choices[0]==='round_trip'?'Round-trip':'One-way')
+    this.choice2 = this.choices.length==2?'One-way':null;
+    this.selectedChoice = this.choice1;
   }
 
+  updateTravelType(travelType:string){
+    this.selectedChoice = travelType;
+    this.onSelect.emit(travelType==='Round-trip'?'round_trip':'one_way');
+  }
+
+  getFontColor(choice:string){
+    return this.selectedChoice===choice?'rgba(255,255,255,0.5)':'#FFFFFF';
+  }
+
+  getBackgroundColor(choice:string){
+    return this.selectedChoice===choice?'#FFFFFF':'rgba(255,255,255,0)';
+  }
 }
