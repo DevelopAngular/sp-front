77d8fbe0d01198a76b209cdb30eb37e6c358807c
William Gulian <williamgulian@gmail.com>
2018-08-17 16:18:23 -0400

    Make prod and staging deployments use their respective configurations

--- scripts/compile_frontend.sh
+++ scripts/compile_frontend.sh
@@ -8,4 +8,12 @@ export PATH="$PATH:$(pwd)/node_modules/.bin"
 deploy_url="${STATIC_URL-/static/}frontend/"
 echo "Frontend deploy url: $deploy_url"
 
-ng build --prod --base-href '/app/' # --deploy-url "$deploy_url"
+config=production
+
+if [ "$CI_ENVIRONMENT_SLUG" = "staging" ]; then
+  config=staging
+fi
+
+echo "Using config: $config"
+
+ng build -c "$config" --base-href '/app/' # --deploy-url "$deploy_url"
