a87a3248b5e8959fb4aef6970f4a7eb2b6da71aa
William Gulian <williamgulian@gmail.com>
2018-08-17 16:15:05 -0400

    Add prod environment and staging environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
 export const environment = {
   production: true,
   serverConfig: {
-    host: 'https://notify-messenger-notify-server-staging.lavanote.com/',
-    client_id: 'aC9QkvU8R5lNxGqjAGxJIWn0B3spLupkvR6vukT3',
-    host_ws: 'wss://notify-messenger-notify-server-staging.lavanote.com/',
+    host: 'https://smartpass.app/',
+    client_id: 'jJBpPq4UJFeUMn5WcmuMx5Fekp3Jp5ogWICt2Y1c',
+    client_secret: 'Zp4MUH0Jb5yEPQoeBbFBJCKxLwIRaTUqMDaAv2nhRkh0CpcpcBDhO2Zqi6MUSwUlf4RqAe5' +
+      'AECZ1sEfhQA9fJm1jDoqaeDkcTT2SokXo0PMyk9adqzs7LkiDvC3PqkEX',
+    host_ws: 'wss://smartpass.app/',
     auth_method: null,
   },
 };
