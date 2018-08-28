39551cc7cf6d76300f5f86cd178126074323916a
Kyle Cook <PlanetCookieX@gmail.com>
2018-08-28 16:11:38 -0400

    secret

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   serverConfig: {
     host: 'https://smartpass.app/',
-    client_id: 'jJBpPq4UJFeUMn5WcmuMx5Fekp3Jp5ogWICt2Y1c',
+    client_id: 'Wmr9cRCU97i8Clp2oaN7Pek8I3C7U7uXLGsJTqPN',
     client_secret: 'kbbJmqWnUxbX3NkBlucHIhHd5Wt9XpcDFuUi9fLVN7ggLjGHknZLz8jVqfwfY3Zxe0o5gNGXRqPPCGBMrjZCn8aQFabNid2MxYYmANxXV2KKg09EvgoNuJA5kDiF2i36',
     host_ws: 'wss://smartpass.app/',
     auth_method: null,
