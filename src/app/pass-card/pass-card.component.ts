7578d422d8db18fec93fda2c5a7af5d0c4be368b
DevelopAngular <angulardeveloper11@gmail.com>
2018-10-23 18:33:55 +0300

    fix console error "not defined property roles"

--- src/app/pass-card/pass-card.component.ts
+++ src/app/pass-card/pass-card.component.ts
@@ -154,9 +154,10 @@ export class PassCardComponent implements OnInit, OnDestroy {
       this.buildPage('Pass Request Accepted', 'by ' +this.getUserName(this.pass.issuer), this.formatDateTime(this.pass.created), (this.pagerPages+1));
     } else if(this.forFuture){
       this.buildPage('Pass Sent', 'by ' +this.getUserName(this.pass.issuer), this.formatDateTime(this.pass.created), (this.pagerPages+1));
-    } else if(!this.pass.issuer.roles.includes('hallpass_student')){
+    } else if(this.pass.issuer) {
+      if(!this.pass.issuer.roles.includes('hallpass_student')){
       this.buildPage('Pass Created', 'by ' +this.getUserName(this.pass.issuer), this.formatDateTime(this.pass.created), (this.pagerPages+1));
-    }
+    }}
 
     if(this.isActive){
       this.buildPage('Pass Started', '', this.formatDateTime(this.pass.created), (this.pagerPages+1));
