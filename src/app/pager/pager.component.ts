126c090681a9168d33059d7b56e09f3180b14ca9
DevelopAngular <angulardeveloper11@gmail.com>
2018-11-05 12:13:32 +0200

    fix redesign pass (SPW-2)

--- src/app/pager/pager.component.ts
+++ src/app/pager/pager.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
 
 @Component({
   selector: 'app-pager',
@@ -6,22 +7,34 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./pager.component.scss']
 })
 export class PagerComponent implements OnInit {
-  
+
   @Input() page = 1;
-  @Input() pages = 2;
+  @Input() pages = 1;
+
+  hideRightButton = new BehaviorSubject(false);
+  hideLeftButton = new BehaviorSubject(true);
 
   constructor() { }
 
-  get $pages(){
-    return Array(this.pages).fill(1).map((x,i)=>(i+1));
+  get $pages() {
+    return Array(this.pages).fill(1).map((x, i) => (i + 1));
   }
 
   ngOnInit() {
   }
 
-  paginator() {
-    if (this.page === this.pages) {
-      this.page = 0;
+  leftPaginator() {
+    this.hideRightButton.next(true);
+    if (this.page === 2) {
+      this.hideLeftButton.next(false);
+    }
+    this.page -= 1;
+  }
+
+  RightPaginator() {
+    this.hideLeftButton.next(true);
+    if (this.page === 1 && this.pages === 2 || this.page === 2 && this.pages === 3 || this.page === 3 && this.pages === 4) {
+      this.hideRightButton.next(false);
     }
     this.page += 1;
   }
