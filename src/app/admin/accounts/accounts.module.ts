c71717b0d062d0913af7c56f578710c1c9e2d1d8
DevelopAngular <angulardeveloper11@gmail.com>
2018-11-23 10:24:27 +0200

    add AdminSharedModule

--- src/app/admin/accounts/accounts.module.ts
+++ src/app/admin/accounts/accounts.module.ts
@@ -7,13 +7,20 @@ import {MainComponent} from './main/main.component';
 import {SharedModule} from '../../shared/shared.module';
 import { GSuiteDialogComponent } from './g-suite-dialog/g-suite-dialog.component';
 import { AdministratorsComponent } from './administrators/administrators.component';
+import {AdminSharedModule} from '../shared/admin-shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     AccountsRoutingModule,
-    SharedModule
+    SharedModule,
+    AdminSharedModule,
   ],
-  declarations: [ AccountsComponent, MainComponent, GSuiteDialogComponent, AdministratorsComponent]
+  declarations: [
+    AccountsComponent,
+    MainComponent,
+    GSuiteDialogComponent,
+    AdministratorsComponent
+  ]
 })
 export class AccountsModule { }
