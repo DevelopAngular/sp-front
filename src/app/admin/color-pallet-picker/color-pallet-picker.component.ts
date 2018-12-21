b856121a4888ab1b21118652493d85f0b45751ce
DevelopAngular <angulardeveloper11@gmail.com>
2018-12-21 16:01:56 +0200

    fix color picker (SPA-256)

--- src/app/admin/color-pallet-picker/color-pallet-picker.component.ts
+++ src/app/admin/color-pallet-picker/color-pallet-picker.component.ts
@@ -1,13 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output, Renderer2, ViewChild } from '@angular/core';
 import { HttpService } from '../../http-service';
 import { map, shareReplay } from 'rxjs/operators';
-import { bumpIn } from '../../animations';
 
 @Component({
     selector: 'app-color-pallet-picker',
     templateUrl: './color-pallet-picker.component.html',
     styleUrls: ['./color-pallet-picker.component.scss'],
-    animations: [bumpIn]
 })
 export class ColorPalletPickerComponent implements OnInit {
 
@@ -21,14 +19,7 @@ export class ColorPalletPickerComponent implements OnInit {
 
   selectedId: number;
 
-  buttonDown = false;
-  hovered: boolean = false;
-
   constructor(private httpService: HttpService, private renderer: Renderer2) { }
-
-  get buttonState() {
-     return this.buttonDown ? 'down' : 'up';
-  }
 
   ngOnInit() {
       this.colors$ = this.httpService.get('v1/color_profiles').pipe(
@@ -44,28 +35,6 @@ export class ColorPalletPickerComponent implements OnInit {
   changeColor(color) {
     this.selectedId = color.id;
     this.selectedEvent.emit(color);
-  }
-
-  onPress(press: boolean) {
-    this.buttonDown = press;
-  }
-
-  onHover(hover: boolean){
-     this.hovered = hover;
-     if (!hover) {
-      this.buttonDown = false;
-     }
-  }
-
-  overEffect(color, ev) {
-    // this.renderer
-    //     .setStyle(ev.nativeElement, 'box-shadow',
-    //         (this.selectedId === color.id ? '0px 0px 50px ' + color.solid_color : '0 2px 4px 0px rgba(0, 0, 0, 0.1)'));
-    // this.selectedId === id ? this.activeShadow += 20 : this.inactiveShadow += 20;
-  }
-
-  leaveEffect(id) {
-    // this.selectedId === id ? this.activeShadow -= 20 : this.inactiveShadow -= 20;
   }
 
 }
