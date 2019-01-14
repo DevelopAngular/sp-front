b8caf4ef23c0666df6e58c036e3884db3d4cd136
William Gulian <williamgulian@gmail.com>
2019-01-14 10:49:04 -0500

    Fix local environment

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   buildType: 'local',
   preferEnvironment: {
-    api_root: 'http://127.0.0.1:8000/',
+    api_root: 'http://127.0.0.1:8000/api/doggo/',
     client_id: 'xDvFSNqxfNwf52eYz8I7SnMJKw9nghifZttfl1ec',
     client_secret: 'feENKl4wWJzDkGu9gQNbevDGBi8ntYoiOrI1Fu1mzOZlZPHBaX7VC2Bz9zwuFHh0tdNz64Dr2iUbMztOao16R2swamHv7w6pppH6uwRZDK4OKa5B80fxaoskMnfHneYh',
     domain: 'example.com',
