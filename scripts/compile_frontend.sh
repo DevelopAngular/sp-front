43a28da01dd657d1de9ead92a1c3173d64fccd25
William Gulian <williamgulian@gmail.com>
2019-01-23 14:00:36 -0500

    Move org flag

--- scripts/compile_frontend.sh
+++ scripts/compile_frontend.sh
@@ -9,7 +9,7 @@ deploy_url="${STATIC_URL-/static/}frontend/"
 echo "Frontend deploy url: $deploy_url"
 
 release_name=$(sentry-cli releases propose-version)
-sentry-cli releases new --project 'hall-pass-web' "$release_name"
+sentry-cli releases --org 'smartpass' new --project 'hall-pass-web' "$release_name"
 
 
 echo 'Generating build-info.ts'
@@ -25,6 +25,6 @@ echo "Using config: $config"
 
 ng build -c "$config" --base-href '/app/' # --deploy-url "$deploy_url"
 
-sentry-cli releases files "$release_name" upload-sourcemaps --org 'smartpass' --rewrite --url-prefix "~/app" ./dist/
+sentry-cli releases --org 'smartpass' files "$release_name" upload-sourcemaps --rewrite --url-prefix "~/app" ./dist/
 
-sentry-cli releases finalize "$release_name"
+sentry-cli releases --org 'smartpass' finalize "$release_name"
