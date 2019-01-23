6cc401a0ba3ee589285faf587672287cea8e56b9
William Gulian <williamgulian@gmail.com>
2019-01-23 13:39:30 -0500

    Setup sentry release creation and sourcemap uploading

--- scripts/compile_frontend.sh
+++ scripts/compile_frontend.sh
@@ -8,6 +8,10 @@ export PATH="$PATH:$(pwd)/node_modules/.bin"
 deploy_url="${STATIC_URL-/static/}frontend/"
 echo "Frontend deploy url: $deploy_url"
 
+release_name=$(sentry-cli releases propose-version)
+sentry-cli releases new "$release_name"
+
+
 echo 'Generating build-info.ts'
 scripts/make_build_info.sh
 
@@ -20,3 +24,7 @@ fi
 echo "Using config: $config"
 
 ng build -c "$config" --base-href '/app/' # --deploy-url "$deploy_url"
+
+sentry-cli releases files "$release_name" upload-sourcemaps --rewrite --url-prefix "~/app" ./dist/
+
+sentry-cli releases finalize "$release_name"
