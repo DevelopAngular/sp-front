02c42dd36eb564205a3f8830a2fd98a9b08f13b2
William Gulian <williamgulian@gmail.com>
2019-01-23 13:56:12 -0500

    Tweak sentry flags

--- scripts/compile_frontend.sh
+++ scripts/compile_frontend.sh
@@ -9,7 +9,7 @@ deploy_url="${STATIC_URL-/static/}frontend/"
 echo "Frontend deploy url: $deploy_url"
 
 release_name=$(sentry-cli releases propose-version)
-sentry-cli releases new --org 'smartpass' --project 'hall-pass-web' "$release_name"
+sentry-cli releases new --project 'hall-pass-web' "$release_name"
 
 
 echo 'Generating build-info.ts'
@@ -27,4 +27,4 @@ ng build -c "$config" --base-href '/app/' # --deploy-url "$deploy_url"
 
 sentry-cli releases files "$release_name" upload-sourcemaps --org 'smartpass' --rewrite --url-prefix "~/app" ./dist/
 
-sentry-cli releases finalize --org 'smartpass' "$release_name"
+sentry-cli releases finalize "$release_name"
