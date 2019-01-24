1b26d8c0709239b9f4b8082d23b4a6c00117373f
denis179210 <denis.bochar1104@gmail.com>
2019-01-24 19:05:05 +0200

    Finished SPA-442,435,436, 432 in progress

--- src/app/school-toggle-bar/school-toggle-bar.component.ts
+++ src/app/school-toggle-bar/school-toggle-bar.component.ts
@@ -28,9 +28,10 @@ export class SchoolToggleBarComponent implements OnInit {
                          this.schools[0];
     this.http.schoolIdSubject.next(this.currentSchool);
   }
-  showOptions(evt: MouseEvent) {
-      const target = new ElementRef(evt.currentTarget);
-      const optionDialog = this.dialog.open(DropdownComponent, {
+  showOptions(target: HTMLElement) {
+      // const target = new ElementRef(evt.currentTarget);
+    console.log('========>', target);
+    const optionDialog = this.dialog.open(DropdownComponent, {
         panelClass: 'consent-dialog-container',
         backdropClass: 'invis-backdrop',
         data: {
