e9913ad4e3bf20bf23df789ea971bfb90bf03409
William Gulian <williamgulian@gmail.com>
2019-02-06 14:39:56 -0500

    Add user-labeling for error (cherry-picked from hot-fixes)

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { ErrorHandler, Injectable } from '@angular/core';
 
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/filter';
@@ -8,6 +8,7 @@ import { interval } from 'rxjs/observable/interval';
 import { race } from 'rxjs';
 import { Observable } from 'rxjs/Observable';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { SentryErrorHandler } from './error-handler';
 import { HttpService } from './http-service';
 import { constructUrl } from './live-data/helpers';
 import { Logger } from './logger.service';
@@ -20,7 +21,9 @@ export class UserService {
   public userData: ReplaySubject<User> = new ReplaySubject<User>(1);
 
   constructor(private http: HttpService,
-              private pollingService: PollingService, private _logging: Logger) {
+              private pollingService: PollingService,
+              private _logging: Logger,
+              private errorHandler: ErrorHandler) {
 
     // this.userData.subscribe(
     //   u => console.log('next user:', u),
@@ -31,6 +34,18 @@ export class UserService {
       .switchMap(() => this.http.get<any>('v1/users/@me'))
       .map(raw => User.fromJSON(raw))
       .subscribe(user => this.userData.next(user));
+
+    if (errorHandler instanceof SentryErrorHandler) {
+      this.userData.subscribe(user => {
+        errorHandler.setUserContext({
+          id: `${user.id}`,
+          email: user.primary_email,
+          is_student: user.isStudent(),
+          is_teacher: user.isStudent(),
+          is_admin: user.isAdmin(),
+        });
+      });
+    }
 
     this.pollingService.listen().subscribe(this._logging.debug);
   }
