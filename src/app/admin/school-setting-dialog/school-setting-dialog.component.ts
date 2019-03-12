15efa2c86cda8e26354ccc0d3c5fffe4a5c4df2c
DevelopAngular <angulardeveloper11@gmail.com>
2019-03-12 11:03:11 +0200

    finished school-settings-dialog (SPA-627)

--- src/app/admin/school-setting-dialog/school-setting-dialog.component.ts
+++ src/app/admin/school-setting-dialog/school-setting-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {MatDialogRef} from '@angular/material';
 
 @Component({
   selector: 'app-school-setting-dialog',
@@ -9,20 +10,33 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class SchoolSettingDialogComponent implements OnInit {
 
   schoolForm: FormGroup;
+  initialState: { display_room: boolean, buffer_time: string | number };
+  changeForm: boolean;
 
-  constructor() { }
+  constructor(private dialogRef: MatDialogRef<SchoolSettingDialogComponent>) { }
 
   ngOnInit() {
     this.buildForm();
-    this.schoolForm.valueChanges.subscribe(res => console.log('FoRM ==>>', res));
+    this.initialState = this.schoolForm.value;
+    this.schoolForm.valueChanges.subscribe(res => {
+      this.changeForm = res.display_room !== this.initialState.display_room || +res.buffer_time !== +this.initialState.buffer_time;
+    });
   }
 
   buildForm() {
     this.schoolForm = new FormGroup({
         display_room: new FormControl(true),
         buffer_time: new FormControl(20,
-            [Validators.pattern('^[0-9]*?[0-9]+$'), Validators.max(59)])
+            [Validators.pattern('^[0-9]*?[0-9]+$'), Validators.max(59), Validators.min(1)])
     });
+  }
+
+  save() {
+
+  }
+
+  close() {
+    this.dialogRef.close();
   }
 
 }
