7a40ac0618d457a06caf74d225e25f28554d5015
DevelopAngular <angulardeveloper11@gmail.com>
2019-03-13 17:39:30 +0200

    advanced options (SPA-630 in Process now restricted)

--- src/app/admin/overlay-container/advanced-options/advanced-options.component.ts
+++ src/app/admin/overlay-container/advanced-options/advanced-options.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {User} from '../../../models/User';
+
+export interface OptionState {
+    state: string;
+    data: {
+      selectedTeachers: User[];
+    };
+}
 
 @Component({
   selector: 'app-advanced-options',
@@ -7,7 +15,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdvancedOptionsComponent implements OnInit {
 
+  @Output() hideBottomBlock: EventEmitter<boolean> = new EventEmitter<boolean>();
+
   openedContent: boolean;
+
+  toggleChoices = [
+      'Any teacher (default)',
+      'Any teachers assigned',
+      'All teachers assigned',
+      'Certain teacher(s)'
+  ];
+
+  optionState: OptionState = {
+      state: this.toggleChoices[0],
+      data: {
+          selectedTeachers: []
+      }
+  };
 
   constructor() { }
 
@@ -16,6 +40,16 @@ export class AdvancedOptionsComponent implements OnInit {
 
   toggleContent() {
     this.openedContent = !this.openedContent;
+  }
+
+  changeState(action, data) {
+    if (action === 'teacher') {
+      this.optionState.data.selectedTeachers = data;
+    }
+  }
+
+  hideEmit(event: boolean) {
+    this.hideBottomBlock.emit(event);
   }
 
 }
