581b0154802c4e296f2b56c00b4c7c06a5ea0ff1
William Gulian <williamgulian@gmail.com>
2019-03-27 00:52:37 -0400

    Add local staging build stage

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -2,13 +2,14 @@ export const environment = {
   production: false,
   buildType: 'local',
   preferEnvironment: {
-    api_root: 'http://127.0.0.1:8000/api/doggo/',
+    api_root: 'http://127.0.0.1:8000/',
     client_id: 'xDvFSNqxfNwf52eYz8I7SnMJKw9nghifZttfl1ec',
     client_secret: 'feENKl4wWJzDkGu9gQNbevDGBi8ntYoiOrI1Fu1mzOZlZPHBaX7VC2Bz9zwuFHh0tdNz64Dr2iUbMztOao16R2swamHv7w6pppH6uwRZDK4OKa5B80fxaoskMnfHneYh',
     domain: 'example.com',
     icon_url: '',
     name: 'Local',
-    ws_url: 'ws://localhost:8053/api/debug/v1/long_polling',
+    ws_url: 'wss://smartpass.app/api/staging/v1/long_polling',
+    // ws_url: 'ws://localhost:8053/api/debug/v1/long_polling',
   },
   firebase: {
     apiKey: 'AIzaSyDKAexSUkOIj63hP9MkLi22CHpykkh_4Bs',
