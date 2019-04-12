162133fd94d73c11cf71d21f620e61f601d0991b
DevelopAngular <angulardeveloper11@gmail.com>
2019-04-12 15:12:34 +0300

    fixed formatDateTime util (SPB-203)

--- src/Util.ts
+++ src/Util.ts
@@ -1,4 +1,5 @@
 ﻿import { TimeService } from './app/services/time.service';
+import * as moment from 'moment';
 
 export class Util{
 
@@ -13,7 +14,8 @@ export class Util{
 
     static formatDateTime(s: Date, timeOnly?: boolean, utc?: boolean) {
         let hours = utc ? s.getUTCHours() : s.getHours();
-        let formattedTime:string = ((hours > 12) ? hours - 12 : hours === 0 ? 12 : hours) + ':' + ((s.getMinutes() < 10) ? '0' : '') + s.getMinutes() + ((hours > 12) ? ' PM' : ' AM');
+        // let formattedTime:string = ((hours > 12) ? hours - 12 : hours === 0 ? 12 : hours) + ':' + ((s.getMinutes() < 10) ? '0' : '') + s.getMinutes() + ((hours > 12) ? ' PM' : ' AM');
+        const formattedTime = moment(s).format('hh:mm A');
         if(timeOnly)
           return formattedTime;
 
