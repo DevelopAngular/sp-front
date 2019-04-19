fe528b86dce66a10e734a458cbab0e2626e50f80
DevelopAngular <angulardeveloper11@gmail.com>
2019-04-19 12:57:51 +0300

    finished (SPA-814)

--- src/app/admin/overlay-container/add-existing-room/add-existing-room.component.ts
+++ src/app/admin/overlay-container/add-existing-room/add-existing-room.component.ts
@@ -1,10 +1,12 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Pinnable} from '../../../models/Pinnable';
+import {bumpIn} from '../../../animations';
 
 @Component({
   selector: 'app-add-existing-room',
   templateUrl: './add-existing-room.component.html',
-  styleUrls: ['./add-existing-room.component.scss']
+  styleUrls: ['./add-existing-room.component.scss'],
+  animations: [bumpIn]
 })
 export class AddExistingRoomComponent implements OnInit {
 
@@ -15,9 +17,19 @@ export class AddExistingRoomComponent implements OnInit {
   @Output() back = new EventEmitter();
   @Output() save = new EventEmitter();
 
+  buttonDown: boolean;
+
   constructor() { }
 
+  get buttonState() {
+      return this.buttonDown ? 'down' : 'up';
+  }
+
   ngOnInit() {
+  }
+
+  onPress(press: boolean) {
+      this.buttonDown = press;
   }
 
   goBack() {
