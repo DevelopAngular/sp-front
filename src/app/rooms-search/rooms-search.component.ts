1d167e78ba5a254fcb15317ec6279daca76b10df
DevelopAngular <developAngular@gmail.com>
2019-04-30 19:03:58 +0300

    get all rooms (SPA-860 in Process)

--- src/app/rooms-search/rooms-search.component.ts
+++ src/app/rooms-search/rooms-search.component.ts
@@ -1,27 +1,59 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from "@angular/core";
 import { LocationsService } from '../services/locations.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import {takeUntil} from "rxjs/internal/operators";
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'app-rooms-search',
   templateUrl: './rooms-search.component.html',
   styleUrls: ['./rooms-search.component.scss']
 })
-export class RoomsSearchComponent implements OnInit {
+export class RoomsSearchComponent implements OnInit, OnDestroy {
 
   @Input() width: string = '280px';
   @Input() placeholder: string;
 
   allRooms;
 
-  constructor(private locationService: LocationsService) { }
+  destroy$ = new Subject();
+
+  constructor(
+      private locationService: LocationsService,
+      private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit() {
     this.locationService.searchLocationsWithConfig('v1/locations?limit=1000&starred=false')
         .subscribe(res => {
           this.allRooms = res.results;
-          debugger;
         });
   }
+
+  ngOnDestroy() {
+      this.destroy$.next();
+      this.destroy$.complete();
+  }
+
+    textColor(item) {
+        if (item.hovered) {
+            return this.sanitizer.bypassSecurityTrustStyle('#1F195E');
+        } else {
+            return this.sanitizer.bypassSecurityTrustStyle('#555558');
+        }
+    }
+
+    getBackground(item) {
+        if (item.hovered) {
+            if (item.pressed) {
+                return '#E2E7F4';
+            } else {
+                return '#ECF1FF';
+            }
+        } else {
+            return '#FFFFFF';
+        }
+    }
 
   onSearch(search) {
 
