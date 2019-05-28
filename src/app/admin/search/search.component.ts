cccd7f63d1e3e54f1f8b5e6cb3060b1464e24f68
DevelopAngular <angulardeveloper11@gmail.com>
2019-05-28 17:10:37 +0300

    fixed dates and duration in search component (SPA-1055)

--- src/app/admin/search/search.component.ts
+++ src/app/admin/search/search.component.ts
@@ -154,31 +154,7 @@ export class SearchComponent implements OnInit {
               url += 'location=' + room.id + '&';
           }
         });
-        // console.log('URL ===>>>>', url);
-      //   console.log('Has rooms\t', this.roomSearchType);
-      //   if (this.roomSearchType == 'Origin') {
-      //     let origins: any[] = this.selectedRooms.map(r => r['id']);
-      //
-      //     Array.from(Array(origins.length).keys()).map(i => {
-      //       url += 'origin=' + origins[i] + '&';
-      //     });
-      //   }
-      //
-      //   if (this.roomSearchType == 'Destination') {
-      //     let destinations: any[] = this.selectedRooms.map(r => r['id']);
-      //
-      //     Array.from(Array(destinations.length).keys()).map(i => {
-      //       url += 'destination=' + destinations[i] + '&';
-      //     });
-      //   }
-      //
-      //   if (this.roomSearchType == 'Either') {
-      //     let locations: any[] = this.selectedRooms.map(r => r['id']);
-      //
-      //     Array.from(Array(locations.length).keys()).map(i => {
-      //       url += 'location=' + locations[i] + '&';
-      //     });
-      //   }
+
       }
       if (this.selectedStudents) {
         let students: any[] = this.selectedStudents.map(s => s['id']);
@@ -210,46 +186,9 @@ export class SearchComponent implements OnInit {
           console.log('DATA', data);
           this.passes = data;
           this.tableData = data.map(hallPass => {
-            // let travelType;
-            //             // if (hallPass.travel_type === 'one_way') {
-            //             //   travelType = `<svg width="15px" height="15px" viewBox="0 0 160 140">
-            //             //                   <title>SP Arrow (Blue-Gray)</title>
-            //             //                   <desc>Created with Sketch.</desc>
-            //             //                   <g id="SP-Arrow-(Blue-Gray)" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            //             //                     <g id="Logo" transform="translate(3.000000, 0.000000)" fill="#7F879D" fill-rule="nonzero">
-            //             //                       <path d="M127.685045,98.1464997 L80.6403711,145.191173 C74.2538542,151.57769 63.9281157,151.606542 57.5771895,145.255616 C51.226263,138.904689 51.2551148,128.578951 57.6416316,122.192434 L104.686305,75.1477605 L57.3461108,27.8075658 C50.9595937,21.4210488 50.9307419,11.0953107 57.2816684,4.74438419 C63.6325949,-1.60654219 73.9583331,-1.57769053 80.34485,4.80882648 L139.415225,63.8792012 C145.703488,70.1674642 145.731895,80.3343446 139.478676,86.5875648 L127.802392,98.2638476 L127.685045,98.1464997 Z M51.229003,99.7185895 L27.7048949,123.242698 C21.3326816,129.614911 11.0584973,129.672126 4.7568622,123.370491 C-1.54477286,117.068856 -1.48755764,106.794672 4.88465572,100.422458 L28.4087639,76.8983503 L4.81684319,53.3064295 C-1.55537032,46.934216 -1.61258544,36.6600316 4.68904964,30.3583967 C10.9906847,24.0567616 21.2648689,24.1139768 27.6370822,30.4861904 L62.7366843,65.5857925 C69.0108639,71.859972 69.0671988,81.9760916 62.862512,88.1807786 L51.2768521,99.7664383 L51.229003,99.7185895 Z" id="Arrow"></path>
-            //             //                     </g>
-            //             //                   </g>
-            //             //                 </svg>`;
-            //             // }
-            //             // if (hallPass.travel_type === 'round_trip' || hallPass.travel_type === 'both') {
-            //             //   travelType = `<svg width="23px" height="11px" viewBox="0 50 150 50">
-            //             //                   <title>SP Arrow Double (Blue-Gray)</title>
-            //             //                   <desc>Created with Sketch.</desc>
-            //             //                   <g id="SP-Arrow-Double-(Blue-Gray)" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            //             //                     <g id="Logo" transform="translate(88.000000, 43.000000)" fill="#7F879D" fill-rule="nonzero">
-            //             //                       <path d="M54.9182725,41.8758399 L34.6840139,61.948234 C31.9371262,64.6731479 27.4959505,64.685458 24.7643707,61.9757295 C22.0327907,59.2660009 22.0452001,54.8603525 24.7920877,52.1354387 L45.0263463,32.0630445 L24.6649821,11.8645614 C21.9180943,9.1396475 21.9056849,4.73399923 24.6372649,2.02427059 C27.3688449,-0.685458001 31.8100205,-0.673147961 34.5569081,2.05176596 L59.9635087,27.2551259 C62.6681366,29.9381181 62.680355,34.275987 59.9907994,36.9440276 L54.9687445,41.9259083 L54.9182725,41.8758399 Z M22.0339692,42.5465982 L11.9160781,52.583551 C9.1753425,55.302362 4.75634063,55.3267739 2.04596125,52.6380763 C-0.664418119,49.9493786 -0.639809431,45.5657266 2.10092618,42.8469156 L12.2188173,32.8099628 L2.07175952,22.7440766 C-0.668976163,20.0252655 -0.693584805,15.6416135 2.01679457,12.9529159 C4.72717394,10.2642183 9.14617576,10.2886301 11.8869114,13.0074412 L26.9835072,27.9832715 C29.6820777,30.6602547 29.7063078,34.9764657 27.0376266,37.6237989 L22.0545494,42.5670137 L22.0339692,42.5465982 Z" id="Arrow"></path>
-            //             //                     </g>
-            //             //                     <g id="Logo" transform="translate(31.000000, 75.000000) scale(-1, 1) translate(-31.000000, -75.000000) translate(0.000000, 43.000000)" fill="#7F879D" fill-rule="nonzero">
-            //             //                       <path d="M54.9182725,41.8758399 L34.6840139,61.948234 C31.9371262,64.6731479 27.4959505,64.685458 24.7643707,61.9757295 C22.0327907,59.2660009 22.0452001,54.8603525 24.7920877,52.1354387 L45.0263463,32.0630445 L24.6649821,11.8645614 C21.9180943,9.1396475 21.9056849,4.73399923 24.6372649,2.02427059 C27.3688449,-0.685458001 31.8100205,-0.673147961 34.5569081,2.05176596 L59.9635087,27.2551259 C62.6681366,29.9381181 62.680355,34.275987 59.9907994,36.9440276 L54.9687445,41.9259083 L54.9182725,41.8758399 Z M22.0339692,42.5465982 L11.9160781,52.583551 C9.1753425,55.302362 4.75634063,55.3267739 2.04596125,52.6380763 C-0.664418119,49.9493786 -0.639809431,45.5657266 2.10092618,42.8469156 L12.2188173,32.8099628 L2.07175952,22.7440766 C-0.668976163,20.0252655 -0.693584805,15.6416135 2.01679457,12.9529159 C4.72717394,10.2642183 9.14617576,10.2886301 11.8869114,13.0074412 L26.9835072,27.9832715 C29.6820777,30.6602547 29.7063078,34.9764657 27.0376266,37.6237989 L22.0545494,42.5670137 L22.0339692,42.5465982 Z" id="Arrow"></path>
-            //             //                     </g>
-            //             //                   </g>
-            //             //                 </svg>`;
-            //             // }
-            // if (hallPass.travel_type === 'both') {
-            //   travelType = `<img src="./assets/SP Arrow Double (Blue-Gray).svg" width="25">`;
-            // }
-            const reportDate = new Date(hallPass.created);
-            const time = reportDate.getHours() <= 12
-              ?
-              `${reportDate.getHours()}:${reportDate.getMinutes() < 10 ? '0' : ''}${reportDate.getMinutes()} ${reportDate.getHours() === 12 ? 'PM' : 'AM'}`
-              :
-              `${reportDate.getHours() - 12}:${reportDate.getMinutes() < 10 ? '0' : ''}${reportDate.getMinutes()} PM`;
-            const prettyReportDate = `${reportDate.getMonth() + 1}/${reportDate.getDate()}  ${time}`;
-            const diff: number = (new Date(hallPass.end_time).getTime() - new Date(hallPass.start_time).getTime()) / 1000;
-            const mins: number = Math.floor(Math.floor(diff) / 60);
-            const secs: number = Math.abs(Math.floor(diff) % 60);
-            const duration = mins + (secs === 0 ? '' : ':') + (secs === 0 ? '' : secs < 10 ? '0' + secs : secs) + ' min';
+
+            const duration = moment.duration(moment(hallPass.end_time).diff(moment(hallPass.start_time))).asMinutes();
+
             const name = hallPass.student.first_name + ' ' + hallPass.student.last_name +
                 ` (${hallPass.student.primary_email.split('@', 1)[0]})`;
             const passes = {
@@ -257,8 +196,8 @@ export class SearchComponent implements OnInit {
                 'Origin': hallPass.origin.title,
                 'TT': hallPass.travel_type,
                 'Destination': hallPass.destination.title,
-                'Date & Time': prettyReportDate,
-                'Duration': duration
+                'Date & Time': moment(hallPass.created).format('M/DD h:mm A'),
+                'Duration': (Number.isInteger(duration) ? duration : duration.toFixed(2)) + ' min'
             };
             Object.defineProperty(passes, 'id', { enumerable: false, value: hallPass.id});
             Object.defineProperty(passes, 'date', {enumerable: false, value: moment(hallPass.created) });
