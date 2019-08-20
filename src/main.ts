750de19ffdeaf245818ec5fe280b694268f20144
Vadym <vadymlazariev@gmail.com>
2019-08-20 17:47:36 +0300

    fcm combines service workers

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,6 @@ console.log(`Frontend build type: ${environment.buildType}`);
 
 function registerSW() {
   if ('serviceWorker' in navigator && environment.production) {
-    console.log('PRODUCTION BUILD');
     navigator.serviceWorker.register('./main-sw.js');
   }
 }
