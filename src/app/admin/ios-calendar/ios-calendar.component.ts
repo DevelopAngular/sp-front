12ec8ebebbbbb7b95ae57a96c45043313b6cacd4
Denis179210 <denis.bochar1104@gmail.com>
2019-08-21 17:07:27 +0300

    Local changes

---
+++ src/app/admin/ios-calendar/ios-calendar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ios-calendar',
+  templateUrl: './ios-calendar.component.html',
+  styleUrls: ['./ios-calendar.component.scss']
+})
+export class IosCalendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
