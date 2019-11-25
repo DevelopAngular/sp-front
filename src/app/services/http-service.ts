59848d3d7644ebd398fb658e461f137ea4bdb2e0
DevelopAngular <angulardeveloper11@gmail.com>
2019-11-25 18:24:39 +0200

    fix school update

--- src/app/services/http-service.ts
+++ src/app/services/http-service.ts
@@ -468,7 +468,7 @@ export class HttpService {
   }
 
   setSchool(school: School) {
-    if (school instanceof School) {
+    if (!!school && school.id) {
       this.storage.setItem('last_school_id', school.id);
     } else {
       this.storage.removeItem('last_school_id');
