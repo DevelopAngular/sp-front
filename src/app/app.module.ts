4a562df90b155dc7c4d8055ab7b7ad16cc8d8108
DevelopAngular <angulardeveloper11@gmail.com>
2019-12-24 11:56:12 +0200

    install angular-oauth2-oidc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -64,6 +64,7 @@ import {ProcessEffects} from './ngrx/onboard-process/effects';
 import { KeyboardShortcutsModule } from 'ng-keyboard-shortcuts';
 import { CoreModule } from './core/core.module';
 import {ScrollHolderDirective} from './scroll-holder.directive';
+import {OAuthModule} from 'angular-oauth2-oidc';
 
 
 const appRoutes: Routes = [
@@ -143,6 +144,7 @@ const appRoutes: Routes = [
         enableTracing: false,
       }
     ),
+    OAuthModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase, 'notifyhallpass'),
     AngularFireMessagingModule,
     AgmCoreModule.forRoot({
