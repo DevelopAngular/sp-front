11e2f44e9bda3cfef6aa4d886b3dd7e2b9f818b3
DevelopAngular <angulardeveloper11@gmail.com>
2020-02-04 13:37:53 +0200

    sign in button fix (SPA-1598)

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {fromEvent} from 'rxjs';
-import {filter, take} from 'rxjs/operators';
+import {filter, take, tap} from 'rxjs/operators';
 import {Router} from '@angular/router';
 import {MatDialog} from '@angular/material';
 import {SignedOutToastComponent} from '../signed-out-toast/signed-out-toast.component';
@@ -23,7 +23,9 @@ export class StorageService {
   detectChanges() {
     fromEvent(window, 'storage')
       .pipe(
-        filter((evt: StorageEvent) => evt.key === 'google_auth' && evt.newValue === null && this.router.url !== ''),
+        filter((evt: StorageEvent) => {
+          return evt.key === 'google_auth' && evt.newValue === null && this.router.url !== '';
+        }),
       )
       .subscribe((evt) => {
         this.matDialog.open(SignedOutToastComponent, {
