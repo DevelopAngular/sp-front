1a277b13f17ba19a87472b19cb52cc7f698c7519
DevelopAngular <angulardeveloper11@gmail.com>
2020-05-07 18:14:47 +0300

    zip file upload and validate image extensions

--- src/app/admin/accounts/profile-picture/profile-picture.component.ts
+++ src/app/admin/accounts/profile-picture/profile-picture.component.ts
@@ -1,9 +1,13 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { fromEvent, of } from 'rxjs';
+
+import {forkJoin, fromEvent, of, zip} from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
+import { isArray, uniqBy, differenceBy } from 'lodash';
+
 import { XlsxService } from '../../../services/xlsx.service';
 import { ZipService } from '../../../services/zip.service';
+import {UserService} from '../../../services/user.service';
 
 @Component({
   selector: 'app-profile-picture',
@@ -14,63 +18,100 @@ export class ProfilePictureComponent implements OnInit {
 
   @ViewChild('csvFile') set fileRef(fileRef: ElementRef) {
     if (fileRef && fileRef.nativeElement) {
-      this.selectedMapFile = fileRef;
-      fromEvent(this.selectedMapFile.nativeElement , 'change')
+      fromEvent(fileRef.nativeElement , 'change')
         .pipe(
           switchMap((evt: Event) => {
-            this.uploadingProgress.csv = true;
+            this.uploadingProgress.csv.inProcess = true;
             const FR = new FileReader();
-            FR.readAsBinaryString(this.selectedMapFile.nativeElement.files[0]);
+            FR.readAsBinaryString(fileRef.nativeElement.files[0]);
             return fromEvent(FR, 'load');
           }),
           map(( res: any) => {
             return this.xlsxService.parseXlSXFile(res);
           }),
+          switchMap(rows => {
+            const validate$ = rows.map(row => {
+              // if (!!row[0]) {
+              //   return this.userService.searchProfileById(row[0]).pipe(
+              //     map(user => {
+              //       debugger;
+              //       return { user_id: row[0], file_name: row[1], isUserId: !!row[0], isFileName: !!row[1], usedId: !user };
+              //     })
+              //   );
+              // } else {
+                return of({ user_id: row[0], file_name: row[1], isUserId: !!row[0], isFileName: !!row[1], usedId: false });
+              // }
+            });
+            return forkJoin(validate$);
+          })
         )
-        .subscribe((users) => {
-          debugger;
+        .subscribe((items) => {
+          this.selectedMapFiles = items;
+          this.uploadingProgress.csv.inProcess = false;
+          this.uploadingProgress.csv.complete = true;
         });
     }
   }
 
   @ViewChild('zip') set zipRef(fileRef: ElementRef) {
     if (fileRef && fileRef.nativeElement) {
-      this.selectedImgFile = fileRef;
       fromEvent(fileRef.nativeElement, 'change')
         .pipe(
           switchMap((event) => {
-            const FR = new FileReader();
-            const file = fileRef.nativeElement.files[0];
-            // FR.readAsBinaryString(fileRef.nativeElement.files[0]);
-            return of(file);
-          }),
-          switchMap((file: any) => {
-            debugger;
-            return this.zipService.getEntries(file);
+            const filesStream = [];
+            this.uploadingProgress.images.inProcess = true;
+            for (let i = 0; i < fileRef.nativeElement.files.length; i++) {
+              const file = fileRef.nativeElement.files.item(i);
+              const extension = file.name.toLowerCase().split('.')[file.name.split('.').length - 1];
+              if (extension === 'zip' || extension === 'jpeg' || extension === 'png') {
+                if (extension === 'zip') {
+                  filesStream.push(this.zipService.loadZip(file));
+                } else if (extension === 'jpeg' || extension === 'png') {
+                  const reader = new FileReader();
+                  reader.readAsDataURL(file);
+                  filesStream.push(fromEvent(reader, 'load').pipe(map((item: any) => {
+                    return { file_name: file.name, file: item.target.result };
+                  })));
+                }
+              }
+            }
+            return zip(...filesStream);
           }),
           map(result => {
-            debugger;
-            return result;
+            let arrayFiles = [];
+            result.forEach(item => {
+              if (isArray(item)) {
+                arrayFiles = [...arrayFiles, ...item];
+              } else {
+                arrayFiles = [...arrayFiles, item];
+              }
+            });
+            return arrayFiles;
           })
         )
-        .subscribe((users) => {
-        debugger;
-      });
+        .subscribe(files => {
+          this.selectedImgFiles = uniqBy(files, 'name');
+          this.uploadingProgress.images.inProcess = false;
+          this.uploadingProgress.images.complete = true;
+        });
     }
   }
 
   page: number = 1;
   form: FormGroup;
-  selectedMapFile;
-  selectedImgFile;
+  selectedMapFiles: {user_id: string | number, file_name: string, isUserId: boolean, isFileName: boolean }[] = [];
+  selectedImgFiles: { file_name: string, file: string }[] = [];
   uploadingProgress = {
-    images: false,
-    csv: false
+    images: { inProcess: false, complete: false, error: null },
+    csv: { inProcess: false, complete: false, error: null }
   };
+  invalidMapFiles;
+  invalidImgFiles;
 
   constructor(
     private xlsxService: XlsxService,
-    private zipService: ZipService
+    private zipService: ZipService,
+    private userService: UserService
   ) { }
 
   ngOnInit() {
@@ -82,6 +123,25 @@ export class ProfilePictureComponent implements OnInit {
 
   nextPage() {
     this.page += 1;
+    if (this.page === 3) {
+      this.findIssues();
+    }
+  }
+
+  findIssues() {
+    this.invalidMapFiles = this.selectedMapFiles.map(file => {
+      const errors = [];
+      if (!file.isFileName) {
+        errors.push({user_id: file.user_id, error: '- Image not listed'});
+      }
+      if (!file.isUserId) {
+        errors.push({image_name: file.file_name, error: '- User ID not listed'});
+      }
+      if (file.isUserId && file.isFileName && !this.selectedImgFiles.find(img => img.file_name === file.file_name)) {
+        errors.push({user_id: file.user_id, image_name: file.file_name, error: '- No image found'});
+      }
+      return errors;
+    });
   }
 
 }
--- src/app/services/zip.service.ts
+++ src/app/services/zip.service.ts
@@ -1,75 +1,71 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { forkJoin, from, of, zip } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
-declare const zip: any;
-
-export interface ZipEntry {
-  version: number;
-  bitFlag: number;
-  compressionMethod: number;
-  lastModDateRaw: number;
-  lastModDate: string;
-  crc32: number;
-  compressedSize: number;
-  uncompressedSize: number;
-  filenameLength: number;
-  extraFieldLength: number;
-  commentLength: number;
-  directory: boolean;
-  offset: 0;
-  filename: string;
-  comment: string;
-}
-
-export interface ZipTaskProgress {
-  active: boolean;
-  current?: number;
-  total?: number;
-}
-
-export interface ZipTask {
-  progress: Observable<ZipTaskProgress>;
-  data: Observable<Blob>;
-}
+declare const JSZip;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ZipService {
 
-  constructor() {
-    zip.workerScriptsPath = 'scripts/';
+  constructor() {}
+
+  loadZip(file) {
+    return from(JSZip.loadAsync(file, {createFolders: false}))
+      .pipe(
+        switchMap((data: any) => {
+          const arrayFiles = [];
+          const arrayNames = [];
+          for (const item in data.files) {
+            const extension = item.split('.')[item.toLowerCase().split('.').length - 1];
+           if (item[0] !== '_' && (extension === 'jpeg' || extension === 'png')) {
+             arrayNames.push(item);
+             arrayFiles.push(from(data.files[item].async('base64')));
+           }
+          }
+          return forkJoin({
+            names: of(arrayNames),
+            files: zip(...arrayFiles)
+          });
+        }),
+        map(({names, files}) => {
+          return names.map((name, i) => {
+            return { file_name: name, file: files[i] };
+          });
+        })
+      );
   }
 
-  getEntries(file): Observable<Array<ZipEntry>> {
-    return new Observable(subscriber => {
-      const reader = new zip.BlobReader(file);
-      zip.createReader(reader, zipReader => {
-        zipReader.getEntries(entries => {
-          subscriber.next(entries);
-          subscriber.complete();
-        });
-      }, message => {
-        subscriber.error({ message });
-      });
-    });
-  }
-
-  getData(entry: ZipEntry): ZipTask {
-    const progress = new Subject<ZipTaskProgress>();
-    const data = new Observable<Blob>(subscriber => {
-      const writer = new zip.BlobWriter();
-
-      // Using `as any` because we don't want to expose this
-      // method in the interface
-      (entry as any).getData(writer, blob => {
-        subscriber.next(blob);
-        subscriber.complete();
-        progress.next(null);
-      }, (current, total) => {
-        progress.next({ active: true, current, total });
-      });
-    });
-    return { progress, data };
-  }
+  // getEntries(file): Observable<Array<ZipEntry>> {
+  //   return new Observable(subscriber => {
+  //     const reader = new zip.BlobReader(file);
+  //     zip.createReader(reader, zipReader => {
+  //       zipReader.getEntries(entries => {
+  //         subscriber.next(entries);
+  //         subscriber.complete();
+  //       });
+  //     }, message => {
+  //       subscriber.error({ message });
+  //     });
+  //   });
+  // }
+  //
+  // getData(entry: ZipEntry): ZipTask {
+  //   const progress = new Subject<ZipTaskProgress>();
+  //   const data = new Observable<Blob>(subscriber => {
+  //     const writer = new zip.BlobWriter();
+  //
+  //     // Using `as any` because we don't want to expose this
+  //     // method in the interface
+  //     (entry as any).getData(writer, blob => {
+  //       subscriber.next(blob);
+  //       subscriber.complete();
+  //       progress.next(null);
+  //     }, (current, total) => {
+  //       progress.next({ active: true, current, total });
+  //     });
+  //   });
+  //   return { progress, data };
+  // }
 }
