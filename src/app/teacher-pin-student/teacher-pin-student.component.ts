15f2a4e4d30a835c88e8833730dd109f81dd5c5b
DevelopAngular <angulardeveloper11@gmail.com>
2020-05-29 16:55:38 +0300

    teacher pin fix layout and change pin

--- src/app/teacher-pin-student/teacher-pin-student.component.ts
+++ src/app/teacher-pin-student/teacher-pin-student.component.ts
@@ -98,6 +98,11 @@ export class TeacherPinStudentComponent implements OnInit, OnDestroy {
                   })
                 );
             }
+          } else if (event.keyCode === 8) {
+            this.pin = this.pin.slice(0, this.pin.length - 1);
+            this.circles[this.pin.length].pressed = false;
+            this.cdr.detectChanges();
+            return of(null);
           }
           return of(null);
         }),
