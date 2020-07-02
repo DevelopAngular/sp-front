9e8fc647c623c8d31eab80142089b0bb202b5942
DevelopAngular <angulardeveloper11@gmail.com>
2020-07-02 16:06:48 +0300

    create page button

--- src/app/admin/explore/explore.component.ts
+++ src/app/admin/explore/explore.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import {MatDialog} from '@angular/material';
+import {PagesDialogComponent} from './pages-dialog/pages-dialog.component';
+import {filter} from 'rxjs/operators';
 
 export interface View {
   [view: string]: CurrentView;
 }
 
 export interface CurrentView {
+  id: number;
   title: string;
   color: string;
   icon: string;
+  action: string;
 }
 
 @Component({
@@ -19,13 +24,32 @@ export interface CurrentView {
 export class ExploreComponent implements OnInit {
 
   views: View = {
-    'pass_search': {title: 'Pass Search', color: '#00B476', icon: './assets/Pass Search (White).svg'}
+    'pass_search': {id: 1, title: 'Pass Search', color: '#00B476', icon: 'Pass Search', action: 'pass_search'},
+    'report_search': {id: 2, title: 'Reports search', color: 'red', icon: 'Report Search', action: 'report_search'},
+    'contact_trace': {id: 3, title: 'Contact trace', color: '#139BE6', icon: 'Contact Trace', action: 'contact_trace'},
+    'rooms_usage': {id: 4, title: 'Rooms Usage', color: 'orange', icon: 'Rooms Usage', action: 'rooms_usage'}
   };
 
   currentView$: BehaviorSubject<string> = new BehaviorSubject<string>('pass_search');
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit() {}
+
+  openSwitchPage(event) {
+    const pagesDialog = this.dialog.open(PagesDialogComponent, {
+      panelClass: 'consent-dialog-container',
+      backdropClass: 'invis-backdrop',
+      data: {
+        'trigger': event.currentTarget,
+        'pages': Object.values(this.views),
+        'selectedPage': this.views[this.currentView$.getValue()]
+      }
+    });
+
+    pagesDialog.afterClosed().pipe(filter(res => !!res)).subscribe(action => {
+      this.currentView$.next(action);
+    });
+  }
 
 }
--- src/app/admin/explore/explore.module.ts
+++ src/app/admin/explore/explore.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { ExploreRoutingModule } from './explore-routing.module';
 import { ExploreComponent } from './explore.component';
-import {CoreModule} from '../../core/core.module';
+import { CoreModule } from '../../core/core.module';
+import { PagesDialogComponent } from './pages-dialog/pages-dialog.component';
 
 @NgModule({
   declarations: [
-    ExploreComponent
+    ExploreComponent,
+    PagesDialogComponent
   ],
   imports: [
     CommonModule,
     ExploreRoutingModule,
     CoreModule
-  ]
+  ],
+  entryComponents: [PagesDialogComponent]
 })
 export class ExploreModule { }
