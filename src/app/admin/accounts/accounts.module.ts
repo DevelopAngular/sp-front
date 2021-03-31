cc33cce82301c8dbb522688ac98dfa0aabe6961d
DevelopAngular <angulardeveloper11@gmail.com>
2021-03-31 18:37:25 +0300

    local commit

--- src/app/admin/accounts/accounts.module.ts
+++ src/app/admin/accounts/accounts.module.ts
@@ -23,6 +23,7 @@ import {AddAccountPopupComponent} from './add-account-popup/add-account-popup.co
 import {BulkAddComponent} from './bulk-add/bulk-add.component';
 import {AddRolePopupComponent} from './select-role/add-role-popup/add-role-popup.component';
 import {PermissionsDialogComponent} from '../accounts-role/permissions-dialog/permissions-dialog.component';
+import {EncounterPreventionDialogComponent} from './encounter-prevention-dialog/encounter-prevention-dialog.component';
 
 @NgModule({
   declarations: [
@@ -46,7 +47,8 @@ import {PermissionsDialogComponent} from '../accounts-role/permissions-dialog/pe
     AddAccountPopupComponent,
     BulkAddComponent,
     AddRolePopupComponent,
-    PermissionsDialogComponent
+    PermissionsDialogComponent,
+    EncounterPreventionDialogComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/admin/accounts/encounter-prevention-dialog/encounter-prevention-dialog.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-encounter-prevention-dialog',
+  templateUrl: './encounter-prevention-dialog.component.html',
+  styleUrls: ['./encounter-prevention-dialog.component.scss']
+})
+export class EncounterPreventionDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
