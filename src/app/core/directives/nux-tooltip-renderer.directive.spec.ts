fab0eddae23571650425d9a5215bf2b246e54a01
DevelopAngular <angulardeveloper11@gmail.com>
2021-12-28 16:58:39 +0200

    fix

--- src/app/core/directives/nux-tooltip-renderer.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import {NuxTooltipRendererDirective} from './nux-tooltip-renderer.directive';
-
-describe('NuxTooltipRendererDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NuxTooltipRendererDirective();
-    expect(directive).toBeTruthy();
-  });
-});
