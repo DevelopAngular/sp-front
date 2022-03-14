dc1e0c745b25cf7717783213f505be013f3a4f82
Qarun-Qadir-Bissoondial <qarun80@hotmail.com>
2022-03-14 12:16:35 -0400

    chore: cypress
    - install cypress and @schematic/cypress
    - writes cypress tests for login page
    - configures `ng e2e` to run cypress
    - creates custom login and logout cypress commands
    - updates gitignore

---
+++ cypress/support/commands.ts
@@ -0,0 +1,61 @@
+// ***********************************************
+// This example namespace declaration will help
+// with Intellisense and code completion in your
+// IDE or Text Editor.
+// ***********************************************
+// declare namespace Cypress {
+//   interface Chainable<Subject = any> {
+//     customCommand(param: any): typeof customCommand;
+//   }
+// }
+//
+// function customCommand(param: any): void {
+//   console.warn(param);
+// }
+//
+// NOTE: You can use it like so:
+// Cypress.Commands.add('customCommand', customCommand);
+//
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// @ts-ignore
+Cypress.Commands.add('login', (username: string, password: string) => {
+  cy.get('div.input-container input[autocomplete="username"]').type(username);
+  cy.get('div.button > app-gradient-button > div.button').click({force: true});
+  cy.get('div.input-container input[autocomplete="password"]').type(password);
+  cy.get('div.button > app-gradient-button > div.button').click({force: true});
+  cy.wait(10000);
+});
+
+// @ts-ignore
+Cypress.Commands.add('logout', () => {
+  cy.get('.options-wrapper > app-white-round-button').click();
+  cy.wait(500);
+  cy.get('div.sign-out').click();
+  cy.wait(5000);
+});
+
