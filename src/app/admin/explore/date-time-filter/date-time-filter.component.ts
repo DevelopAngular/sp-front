54d66455969f0cead120c6635e690635ab03b20a
DevelopAngular <angulardeveloper11@gmail.com>
2022-03-17 15:35:20 +0200

    local commit

--- src/app/admin/explore/date-time-filter/date-time-filter.component.ts
+++ src/app/admin/explore/date-time-filter/date-time-filter.component.ts
@@ -56,6 +56,7 @@ export class DateTimeFilterComponent implements OnInit {
   }
 
   calendarResult(dates) {
+    debugger;
       this._matDialogRef.close({ date: dates, options: this.options });
   }
 
