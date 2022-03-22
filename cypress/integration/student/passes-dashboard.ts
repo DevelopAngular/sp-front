0f938e21ee21389cef3829bd9ceb84b4a3ca1ea4
Qarun-Qadir-Bissoondial <qarun80@hotmail.com>
2022-03-22 14:29:17 -0400

    test(e2e-pass-request): denied pass request
    adds tests for denied passes (student)

--- cypress/integration/student/passes-dashboard.ts
+++ cypress/integration/student/passes-dashboard.ts
@@ -178,6 +178,13 @@ describe('Student - Passes Dashboard', () => {
 
   describe.only('Request Pass', () => {
     const requestPassMessage = 'Some Message';
+    const deniedPassMessage = 'Denied due to reasons';
+
+    after(() => {
+      cy.get('div.pass-card-header app-icon-button div.icon-button-container').click({force: true});
+      cy.get('app-consent-menu div.option-wrapper').contains('Delete Pass Request').parent().click({force: true});
+    })
+
     it('should request a "now" pass with a message', () => {
       cy.visit('http://localhost:4200/main/passes');
       PassFunctions.openCreatePassDialog('now');
@@ -236,12 +243,92 @@ describe('Student - Passes Dashboard', () => {
       endPass();
     });
 
-    // it('should receive a denied pass request', () => {
-    //
-    // });
-    //
-    // it('should re-try a denied pass request', () => {
-    //
-    // });
+    it('should receive a denied pass request', () => {
+      PassFunctions.openCreatePassDialog('now');
+      PassFunctions.selectCurrentRoom('Bathroom');
+      cy.get('img[alt="LOCK"].lock').parent().click();
+      PassFunctions.searchForTeacher('demoteacher1');
+      cy.get('textarea.message-box').type(requestPassMessage);
+      cy.get('div.rest-mes-content app-gradient-button div.button').click();
+      PassFunctions.setMinimumPassDuration();
+      cy.get('div.request-button-content').click();
+      cy.wait(1000);
+      cy.get('app-inline-request-card')
+        .should('exist')
+        .should('be.visible')
+        .should('have.length', 1);
+
+      // @ts-ignore
+      cy.logout(); cy.login(Cypress.env('teacherUsername'), Cypress.env('teacherPassword'));
+      cy.get('div.main-page-right app-pass-tile div.tile-wrapper').first().click();
+      cy.get('app-request-card div.paginator-button app-icon-button div.icon-button-container').click({force: true});
+      cy.get('app-request-card div.message span').last().should('have.text', requestPassMessage);
+      cy.get('app-request-card div.paginator-button app-icon-button div.icon-button-container').click({force: true});
+
+      cy.get('app-request-card div.header-content app-icon-button div.icon-button-container').first().click();
+      cy.get('app-consent-menu div.option-wrapper').contains('Deny Pass Request').parent().click();
+
+      cy.get('.options-wrapper div.icon-button-container').first().click();
+      cy.get('div.sign-out').click();
+      cy.wait(5000);
+      // @ts-ignore
+      cy.login(Cypress.env('studentUsername'), Cypress.env('studentPassword'));
+
+      cy.get('app-inline-request-card')
+        .should('exist')
+        .should('be.visible')
+        .should('have.length', 1);
+
+      cy.get('app-inline-request-card div.resend-button-content')
+        .should('exist')
+        .should('be.visible')
+        .should('have.length', 1);
+
+      cy.get('app-inline-request-card div.resend-button-content-subtitle').should('have.text', 'Pass Request Denied');
+    });
+
+    it('should re-try a denied pass request', () => {
+      cy.get('app-inline-request-card div.resend-button-content')
+        .should('exist')
+        .should('be.visible')
+        .should('have.length', 1)
+        .click({force: true});
+
+      cy.get('app-inline-request-card div.resend-button-content-title').should('contain.text', 'Enter Teacher Pin');
+      cy.get('app-inline-request-card div.resend-button-content div.description-title').should('exist');
+    });
+
+    it('should receive a denied pass request with a message', () => {
+      // @ts-ignore
+      cy.logout(); cy.login(Cypress.env('teacherUsername'), Cypress.env('teacherPassword'));
+      cy.get('div.main-page-right app-pass-tile div.tile-wrapper').first().click();
+      cy.get('app-request-card div.paginator-button app-icon-button div.icon-button-container').click({force: true});
+      cy.get('app-request-card div.message span').last().should('have.text', requestPassMessage);
+      cy.get('app-request-card div.paginator-button app-icon-button div.icon-button-container').click({force: true});
+
+      cy.get('app-request-card div.header-content app-icon-button div.icon-button-container').first().click();
+      cy.get('app-consent-menu div.option-wrapper').contains('Attach Message & Deny').parent().click();
+
+      cy.get('div.content-wrapper textarea').clear().type(deniedPassMessage);
+      cy.get('div.rest-mes-wrapper app-gradient-button div.button').click({force: true});
+
+      cy.get('.options-wrapper div.icon-button-container').first().click();
+      cy.get('div.sign-out').click();
+      cy.wait(5000);
+      // @ts-ignore
+      cy.login(Cypress.env('studentUsername'), Cypress.env('studentPassword'));
+
+      cy.get('app-inline-request-card')
+        .should('exist')
+        .should('be.visible')
+        .should('have.length', 1);
+
+      cy.get('app-inline-request-card div.resend-button-content')
+        .should('exist')
+        .should('be.visible')
+        .should('have.length', 1);
+
+      cy.get('app-inline-request-card div.resend-button-content-subtitle').should('have.text', 'Pass Request Denied');
+    });
   });
 });
