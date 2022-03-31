8057a09bc010abc8b7d094d8949ac5f536acebca
Dhruv Sringari <dhruvy427@gmail.com>
2022-03-30 23:49:59 -0700

    update firebase config

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
 
 firebase.initializeApp({
-  'messagingSenderId': '625620388494'
+  apiKey: "AIzaSyDKAexSUkOIj63hP9MkLi22CHpykkh_4Bs",
+  authDomain: "notifyhallpass.firebaseapp.com",
+  databaseURL: "https://notifyhallpass.firebaseio.com",
+  projectId: "notifyhallpass",
+  storageBucket: "notifyhallpass.appspot.com",
+  messagingSenderId: "625620388494",
+  appId: "1:625620388494:web:2205663799869a91"
 });
 
 const messaging = firebase.messaging();
