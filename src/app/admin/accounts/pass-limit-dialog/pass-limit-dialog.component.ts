2e0c1b7365121c91a595b32a9b9cc8849558192e
Qarun-Qadir-Bissoondial <qarun80@hotmail.com>
2022-04-15 16:59:09 -0400

    feat(pass-limits-admin): limit frequency
    creates pass limit frequency dropdown
    creates error message for badly format passes string

--- src/app/admin/accounts/pass-limit-dialog/pass-limit-dialog.component.ts
+++ src/app/admin/accounts/pass-limit-dialog/pass-limit-dialog.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
-import {MatDialogRef} from '@angular/material/dialog';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 import {BehaviorSubject} from 'rxjs';
 import {CreateFormService} from '../../../create-hallpass-forms/create-form.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-pass-limit-dialog',
@@ -21,21 +22,22 @@ export class PassLimitDialogComponent implements OnInit {
 
   passLimitForm = new FormGroup({
     enabled: new FormControl(false),
-    limits: new FormControl(null, Validators.min(1)),
-    frequency: new FormControl('week', Validators.required)
+    limits: new FormControl('5 passes', Validators.pattern(/^((1 pass)|(\d+ passes))$/)),
+    frequency: new FormControl('day', Validators.required)
   }); // TODO: disable while fetching the pass limit status
   // enabledControl = new FormControl(false); // TODO: get value from server
   // limitsControl = new FormControl(null, Validators.min(1));  // TODO: get value from server
   individualLimits: any[] = [];
+  private frequencyDialogRef: MatDialogRef<HTMLElement, any>;
+  showLimitFormatError = false;
 
   constructor(
+    private dialog: MatDialog,
     public dialogRef: MatDialogRef<PassLimitDialogComponent>,
     private formService: CreateFormService
   ) { }
 
   ngOnInit(): void {
-    // this.passLimitForm.addControl('enabled', this.enabledControl);
-    // this.passLimitForm.addControl('limits', this.limitsControl);
     this.frameMotion$ = this.formService.getFrameMotionDirection();
   }
 
@@ -45,5 +47,28 @@ export class PassLimitDialogComponent implements OnInit {
 
   toggleMainPassLimits(enabled: boolean) {
     this.passLimitForm.patchValue({ enabled });
+  }
+
+  triggerFrequencyDialog() {
+    const freqButton = document.querySelector('#passLimitFrequencyButton');
+    const coords = freqButton.getBoundingClientRect();
+    this.frequencyDialogRef = this.dialog.open(this.frequencyPopup, {
+      hasBackdrop: true,
+      backdropClass: 'cdk-overlay-transparent-backdrop',
+      closeOnNavigation: true,
+      restoreFocus: true,
+      panelClass: 'pass-limits-frequency-dialog',
+      position: {
+        top: `${coords.bottom}px`,
+        left: `${coords.left}px`
+      }
+    });
+    this.frequencyDialogRef.afterClosed().pipe(filter(Boolean)).subscribe(frequency => {
+      this.passLimitForm.patchValue({frequency});
+    });
+  }
+
+  selectFrequency(frequency: string) {
+    this.frequencyDialogRef.close(frequency);
   }
 }
