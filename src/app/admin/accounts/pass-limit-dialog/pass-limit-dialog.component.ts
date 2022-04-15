6ac5698b8b26a200b73aff80d241766bf1ebe4e5
Qarun-Qadir-Bissoondial <qarun80@hotmail.com>
2022-04-15 15:05:08 -0400

    feat(pass-limits): school pass limits
    creates school pass limit section
    adds square button for pass limit on admin accounts dashboard

--- src/app/admin/accounts/pass-limit-dialog/pass-limit-dialog.component.ts
+++ src/app/admin/accounts/pass-limit-dialog/pass-limit-dialog.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 import {MatDialogRef} from '@angular/material/dialog';
 import {BehaviorSubject} from 'rxjs';
 import {CreateFormService} from '../../../create-hallpass-forms/create-form.service';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-pass-limit-dialog',
@@ -10,15 +10,22 @@ import {FormControl, FormGroup} from '@angular/forms';
   styleUrls: ['./pass-limit-dialog.component.scss']
 })
 export class PassLimitDialogComponent implements OnInit {
+  @ViewChild('passLimitsFrequencyDialog') frequencyPopup: TemplateRef<HTMLElement>;
+
   pageNumber = 1;
   frameMotion$: BehaviorSubject<any>;
   showInfoMessage = true; // TODO: hide this message based on database value in the future
   passLimitToggleTooltip = `Some help text about pass limits`; // TODO: Get text for this
   individualLimitsTooltop = `Some help text about individual limits`; // TODO: Get text for this
   individualStudentLimits = [];
-  passLimitForm = new FormGroup({ // TODO: disable while fetching the pass limit status
-    enable: new FormControl(false) // TODO: get value from server
-  });
+
+  passLimitForm = new FormGroup({
+    enabled: new FormControl(false),
+    limits: new FormControl(null, Validators.min(1)),
+    frequency: new FormControl('week', Validators.required)
+  }); // TODO: disable while fetching the pass limit status
+  // enabledControl = new FormControl(false); // TODO: get value from server
+  // limitsControl = new FormControl(null, Validators.min(1));  // TODO: get value from server
   individualLimits: any[] = [];
 
   constructor(
@@ -27,10 +34,16 @@ export class PassLimitDialogComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    // this.passLimitForm.addControl('enabled', this.enabledControl);
+    // this.passLimitForm.addControl('limits', this.limitsControl);
     this.frameMotion$ = this.formService.getFrameMotionDirection();
   }
 
   addIndividualLimit() {
 
+  }
+
+  toggleMainPassLimits(enabled: boolean) {
+    this.passLimitForm.patchValue({ enabled });
   }
 }
