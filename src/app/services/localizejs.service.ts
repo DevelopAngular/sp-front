259d4ad68df8233c2027e104da80bfaec84f9ef2
innermond <innermond@yahoo.com>
2022-04-18 15:56:54 +0300

    reducing unwanted translations

--- src/app/services/localizejs.service.ts
+++ src/app/services/localizejs.service.ts
@@ -106,10 +106,11 @@ export class LocalizejsService {
           '!function(a){if(!a.Localize){a.Localize={};for(var e=["translate","untranslate","phrase","initialize","translatePage","setLanguage","getLanguage","getSourceLanguage","detectLanguage","getAvailableLanguages","untranslatePage","bootstrap","prefetch","on","off","hideWidget","showWidget"],t=0;t<e.length;t++)a.Localize[e[t]]=function(){}}}(window);',
           `Localize.initialize({
             key: '${key}',
-            rememberLanguage: true,
+            rememberLanguage: false,
             autoApprove: true,
             retranslateOnNewPhrases: true,
-            translateTimeElement: true,
+            saveNewPhrasesFromSource: false,
+            translateTimeElement: false,
             hideWidget: true,
           });
           Localize.hideWidget();
