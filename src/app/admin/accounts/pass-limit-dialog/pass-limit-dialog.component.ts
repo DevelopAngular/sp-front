ed870633e175fa5f798896d8b6aee0dbe2316c9e
Qarun-Qadir-Bissoondial <qarun80@hotmail.com>
2022-04-23 00:25:16 -0400

    feat(pass-limits-admin): school-wide limits
    finishes school-wide pass limit UI

--- src/app/admin/accounts/pass-limit-dialog/pass-limit-dialog.component.ts
+++ src/app/admin/accounts/pass-limit-dialog/pass-limit-dialog.component.ts
@@ -1,17 +1,32 @@
-import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 import {BehaviorSubject} from 'rxjs';
 import {CreateFormService} from '../../../create-hallpass-forms/create-form.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {filter} from 'rxjs/operators';
+import {NextStep} from '../../../animations';
+import {ScreenService} from '../../../services/screen.service';
+
+/**
+ * TODOS for individual pass limits
+ * TODO: Add student label with Student search
+ * TODO: Hook up output of student search component with new individual limit form
+ * TODO: Make the number of limits input into it's own component
+ * TODO: Put a Number of limits per day
+ * TODO: Put the description input
+ * TODO: Hook up inputs to forms
+ * TODO: Put the save button on the top right of the new individual form header
+ * TODO: Create shared component for pass limit drop down
+ */
 
 @Component({
   selector: 'app-pass-limit-dialog',
   templateUrl: './pass-limit-dialog.component.html',
-  styleUrls: ['./pass-limit-dialog.component.scss']
+  styleUrls: ['./pass-limit-dialog.component.scss'],
+  animations: [NextStep]
 })
 export class PassLimitDialogComponent implements OnInit {
-  @ViewChild('passLimitsFrequencyDialog') frequencyPopup: TemplateRef<HTMLElement>;
+  // TODO: This is for when multiple pass frequencies are implemented
+  // @ViewChild('passLimitsFrequencyDialog') frequencyPopup: TemplateRef<HTMLElement>;
 
   pageNumber = 1;
   frameMotion$: BehaviorSubject<any>;
@@ -25,15 +40,12 @@ export class PassLimitDialogComponent implements OnInit {
     limits: new FormControl('5 passes', Validators.pattern(/^((1 pass)|(\d+ passes))$/)),
     frequency: new FormControl('day', Validators.required)
   }); // TODO: disable while fetching the pass limit status
-  // enabledControl = new FormControl(false); // TODO: get value from server
-  // limitsControl = new FormControl(null, Validators.min(1));  // TODO: get value from server
-  individualLimits: any[] = [];
-  private frequencyDialogRef: MatDialogRef<HTMLElement, any>;
   showLimitFormatError = false;
 
   constructor(
     private dialog: MatDialog,
     public dialogRef: MatDialogRef<PassLimitDialogComponent>,
+    public screenService: ScreenService,
     private formService: CreateFormService
   ) { }
 
@@ -41,34 +53,32 @@ export class PassLimitDialogComponent implements OnInit {
     this.frameMotion$ = this.formService.getFrameMotionDirection();
   }
 
-  addIndividualLimit() {
-
-  }
-
   toggleMainPassLimits(enabled: boolean) {
     this.passLimitForm.patchValue({ enabled });
   }
 
-  triggerFrequencyDialog() {
-    const freqButton = document.querySelector('#passLimitFrequencyButton');
-    const coords = freqButton.getBoundingClientRect();
-    this.frequencyDialogRef = this.dialog.open(this.frequencyPopup, {
-      hasBackdrop: true,
-      backdropClass: 'cdk-overlay-transparent-backdrop',
-      closeOnNavigation: true,
-      restoreFocus: true,
-      panelClass: 'pass-limits-frequency-dialog',
-      position: {
-        top: `${coords.bottom}px`,
-        left: `${coords.left}px`
-      }
-    });
-    this.frequencyDialogRef.afterClosed().pipe(filter(Boolean)).subscribe(frequency => {
-      this.passLimitForm.patchValue({frequency});
-    });
-  }
+  // TODO: This is for when multiple pass frequencies are implemented
+  // triggerFrequencyDialog() {
+  //   const freqButton = document.querySelector('#passLimitFrequencyButton');
+  //   const coords = freqButton.getBoundingClientRect();
+  //   this.frequencyDialogRef = this.dialog.open(this.frequencyPopup, {
+  //     hasBackdrop: true,
+  //     backdropClass: 'cdk-overlay-transparent-backdrop',
+  //     closeOnNavigation: true,
+  //     restoreFocus: true,
+  //     panelClass: 'pass-limits-frequency-dialog',
+  //     position: {
+  //       top: `${coords.bottom}px`,
+  //       left: `${coords.left}px`
+  //     }
+  //   });
+  //   this.frequencyDialogRef.afterClosed().pipe(filter(Boolean)).subscribe(frequency => {
+  //     this.passLimitForm.patchValue({frequency});
+  //   });
+  // }
 
-  selectFrequency(frequency: string) {
-    this.frequencyDialogRef.close(frequency);
-  }
+  // TODO: This is for when multiple pass frequencies are implemented
+  // selectFrequency(frequency: string) {
+  //   this.frequencyDialogRef.close(frequency);
+  // }
 }
