d3c8895581e8888bd98e886fb1bdb39257929b49
innermond <innermond@yahoo.com>
2022-06-08 21:17:43 +0300

    admin change status

--- cypress/integration/admin/reports.ts
+++ cypress/integration/admin/reports.ts
@@ -49,7 +49,6 @@ describe('Admin - Reports',  () => {
           // try to find an a row with a pass tile
           cy.get('app-sp-data-table table tbody tr').then($rows => {
             const maybePassTiles = $rows.find('td div.pass-icon:eq(0)');
-            cy.log(maybePassTiles)
             const hasPassTile = maybePassTiles.length > 0;
             // choose a row
             let $row;
@@ -61,22 +60,34 @@ describe('Admin - Reports',  () => {
             
             const wrap = cy.wrap($row);
             wrap.get('td:eq(0)').should('be.visible').click();
-            cy.get('mat-dialog-container app-report-info-dialog').should('be.visible');         
-            cy.get('div[class~="cdk-overlay-backdrop"]').click({force: true});
+            cy.get('mat-dialog-container app-report-info-dialog app-status-chip').should('be.visible').then($chip => {
+              const status = $chip.text();
+              cy.wrap($chip).click();
+              cy.intercept({
+                method: 'PATCH',
+                url: /^https\:\/\/smartpass\.app\/api\/prod\-us\-central\/v1\/event_reports\/\d+$/
+              }).as('statuschange');
+              cy.get('app-status-editor app-status-chip span:not([class~='+status+'])').click();
+              cy.wait('@statuschange').its('response').then(res => {
+                expect(res.headers).to.include({'content-type': 'application/json'});
+                expect(res.statusCode).to.equal(200);
+              });
+            });         
+            
+            cy.get('div[class~="cdk-overlay-backdrop"]').click({force: true, multiple: true});
             // foud passtile then test it
             if (hasPassTile) {
               wrap.get('td:eq(3) > div.pass-icon').then($el => {
                 if (!!$el) {
                   cy.wrap($el).click();
                   cy.get('app-pass-card').should('be.visible');
-                  cy.get('div[class~="cdk-overlay-backdrop"]').click({force: true});
+                  cy.get('div[class~="cdk-overlay-backdrop"]').click({force: true, multiple: true});
                 }
               });
             }
           });
         }
       );
-      expect(true).to.equal(true);
     });
   });
 });
