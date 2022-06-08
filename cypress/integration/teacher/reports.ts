b73f89c6191631b93b1f9bc323e21f9a14b6d07d
innermond <innermond@yahoo.com>
2022-06-08 15:12:27 +0300

    teacher reports a pass

--- cypress/integration/teacher/reports.ts
+++ cypress/integration/teacher/reports.ts
@@ -1,39 +1,26 @@
+import {closeModal} from '../../support/functions/general';
+
 describe('Teacher - Reports',  () => {
   const timeout = 20000;
 
   before(() => {
-    cy.intercept({
-      method: 'GET',
-      url: 'https://smartpass.app/api/prod-us-central/v1/hall_passes?**'
-    }).as('hallpasses');
       // @ts-ignore
     cy.login(Cypress.env('teacherUsername'), Cypress.env('teacherPassword'));
-    cy.visit('http://localhost:4200/main/hallmonitor');
-    cy.wait('@hallpasses', {timeout});
   });
 
   after(()=> {
-  
+    cy.logoutTeacher();
   });
-
-  beforeEach(() => {
-  
-  });
-
-  afterEach(() => {
-  
-  });
-
-  const getExplorePanel = (): Cypress.Chainable<JQuery<HTMLElement>> => {
-    return cy.get('app-nav app-nav-button').contains('Explore');
-  };
-
-  const getReports = (): Cypress.Chainable<JQuery<HTMLElement>> => {
-    return cy.get('mat-dialog-container > app-pages-dialog div.title').contains('Reports');
-  } 
 
   describe('Report', () => {
     it('should expects a teacher to search for a student and report him to admin', () => {
+      cy.intercept({
+        method: 'GET',
+        url: 'https://smartpass.app/api/prod-us-central/v1/hall_passes?**'
+      }).as('hallpasses');
+      cy.visit('http://localhost:4200/main/hallmonitor');
+      cy.wait('@hallpasses', {timeout});
+
       cy.intercept({
         method: 'GET',
         url: 'https://smartpass.app/api/prod-us-central/v1/users?role=_profile_student**'
@@ -44,6 +31,8 @@ describe('Teacher - Reports',  () => {
       // search students to be reported
       cy.get('app-report-form app-round-input input[placeholder="Search students"]').type('demo');
       cy.wait('@searchstudents', {timeout});
+
+      cy.get('app-report-form div[class~=divider] app-white-button').should('not.exist');
 
       // assumed we have students found!
       // choose a studewnt
@@ -64,6 +53,44 @@ describe('Teacher - Reports',  () => {
       });
 
     });
+    
+    it('should a teacher reports a pass', () => {
+
+      cy.intercept({
+        method: 'GET',
+        url: 'https://smartpass.app/api/prod-us-central/v1/locations?limit=**'
+      }).as('locations');
+
+      // press home tab
+      cy.get('app-navbar app-nav-button').contains('Home').click();
+      cy.wait('@locations', {timeout});
+
+      // press the first pass tile that exists
+      // TODO ensures that a tile exists otherwise create it
+      cy.get('app-pass-collection app-pass-tile').first().click();
+      // report button on pass popup
+      cy.get('app-pass-card app-student-passes app-gradient-button').contains('Report').should('exist').click();
+
+      cy.get('mat-dialog-container app-report-form div[class~=divider] app-white-button').should('exist');
+
+      // set up a specific message
+      cy.get('app-report-form textarea').type('TEST_REPORT_STUDENT');
+
+      cy.intercept({
+        method: 'POST',
+        url: 'https://smartpass.app/api/prod-us-central/v1/event_reports/bulk_create'
+      }).as('reportstudents');
+
+      // submit report
+      cy.get('mat-dialog-container app-report-form div[class~=divider] app-white-button').click();
+      cy.wait('@reportstudents', {timeout}).its('response').then(res => {
+        expect(res.headers).to.include({'content-type': 'application/json'});
+        expect(res.statusCode).to.equal(200);
+        closeModal();
+      });
+
+    });
+
   });
 
 });
