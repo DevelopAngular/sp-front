b327603f7c8b88237f753df804bf566efc4d6a38
innermond <innermond@yahoo.com>
2022-06-15 23:01:54 +0300

    init

---
+++ src/app/admin/overlay-container/visibility-room/visibility-room.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visibility-room',
+  templateUrl: './visibility-room.component.html',
+  styleUrls: ['./visibility-room.component.scss']
+})
+export class VisibilityRoomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
