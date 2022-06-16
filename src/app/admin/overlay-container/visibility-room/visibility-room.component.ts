d36cd187b95efbfdc7dd62245bda4bb9adfd5770
innermond <innermond@yahoo.com>
2022-06-16 23:16:16 +0300

    styling+icon+tooltip

--- src/app/admin/overlay-container/visibility-room/visibility-room.component.ts
+++ src/app/admin/overlay-container/visibility-room/visibility-room.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+type VisibilityMode = 'visible_all_students' | 'visible_certain_students' | 'hidden_certain_students';
+
 @Component({
   selector: 'app-visibility-room',
   templateUrl: './visibility-room.component.html',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisibilityRoomComponent implements OnInit {
 
-  constructor() { }
+  mode: VisibilityMode = 'visible_all_students'; 
+  mapMode: Record<VisibilityMode, string> = {
+    'visible_all_students': 'Show room for all students',
+    'visible_certain_students': 'Show room for certain students',
+    'hidden_certain_students': 'Hide room for certain students',
+  };
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  tooltipText: string = 'Change room visibility';
+
+  ngOnInit(): void {}
 
 }
