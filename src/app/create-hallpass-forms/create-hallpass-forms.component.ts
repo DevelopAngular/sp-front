be364decff60c93f5f06f317452c4030a3646189
Qarun-Qadir-Bissoondial <qarun80@hotmail.com>
2022-06-22 01:44:18 -0400

    ui(pass-limits): ui bug fixes
    - removes the New badge from NUX
    - updates NUX badge styles
    - includes bottom pass limits banner on pass creation form
    - creates new component for pass limits banner
    - adjusts scaling calculations for larger-screen pass limits view

--- src/app/create-hallpass-forms/create-hallpass-forms.component.ts
+++ src/app/create-hallpass-forms/create-hallpass-forms.component.ts
@@ -1,4 +1,32 @@
 import {Component, OnInit} from '@angular/core';
+import {User} from '../models/User';
+import {Location} from '../models/Location';
+import {PassLimitInfo} from '../models/HallPassLimits';
+
+// TODO: Get proper types for fields and complete fields
+export interface CreatePassDialogData {
+  forLater: boolean;
+  forStaff: boolean;
+  forInput: boolean;
+  kioskMode: boolean;
+  forKioskMode: boolean;
+  fromAdmin: boolean;
+  adminSelectedStudent: User;
+  kioskModeSelectedUser: User[];
+  teacher: User;
+  kioskModeRoom: Location;
+  originalFromLocation: Location;
+  originalToLocation: Location;
+  gradient: string;
+  request: any;
+  missedRequest: boolean;
+  resend_request: boolean;
+  request_time: any;
+  isDeny: boolean;
+  entryState: { step: number; state: number; };
+  hasClose: boolean;
+  passLimitInfo: PassLimitInfo;
+}
 
 @Component({
   selector: 'app-create-hallpass-forms',
@@ -7,8 +35,10 @@ import {Component, OnInit} from '@angular/core';
 })
 export class CreateHallpassFormsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
 }
