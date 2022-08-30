5cd71674620c28a5d7f8fedb94330644fa045205
innermond <innermond@yahoo.com>
2022-08-30 12:30:06 +0300

    WIP: read file

--- src/app/admin/overlay-container/visibility-room/import-student-list/import-student-list.component.ts
+++ src/app/admin/overlay-container/visibility-room/import-student-list/import-student-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, EventEmitter, Input, OnInit, OnDestroy, Output, ViewChild} from '@angular/core';
 import {fromEvent, Observable, Subject} from 'rxjs';
 import {tap, map, switchMap, takeUntil} from 'rxjs/operators';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-import-student-list',
@@ -10,19 +11,26 @@ import {tap, map, switchMap, takeUntil} from 'rxjs/operators';
 export class ImportStudentListComponent implements OnInit, OnDestroy {
 
   @ViewChild('dropArea') dropArea: ElementRef;
-  @ViewChild('file', { static: true }) set fileRef(fileRef: ElementRef) {
-    if (!fileRef?.nativeElement) {return;}
-
-    fromEvent(fileRef.nativeElement, 'change').pipe(
-      tap(v => console.log(v)),
-      takeUntil(this.destroy$)
-    )
-  }
+  @ViewChild('file', { static: true }) fileRef: ElementRef;
 
   destroy$ = new Subject();
   constructor() { }
 
   ngOnInit(): void {
+    fromEvent(this.fileRef.nativeElement, 'change').pipe(
+      switchMap(_ => {
+        const fr = new FileReader();
+        fr.readAsBinaryString(this.fileRef.nativeElement.files[0]);
+        return fromEvent(fr, 'load');
+      }),
+      map((evt: ProgressEvent) => {
+        console.log(evt);
+        // TODO check is right type of file?
+        // create a workbook
+        const wb = XLSX.read(evt.target.result, {type: 'binary'}); 
+      }),
+      //takeUntil(this.destroy$)
+    ).subscribe();
   }
 
   ngOnDestroy(): void {
