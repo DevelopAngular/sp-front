14606cd77966531504569fb20e4938ce3c08572b
innermond <innermond@yahoo.com>
2022-12-12 20:42:42 +0200

    fix tooltip + check disabled options dim

--- src/app/notification-form/notification-option/notification-option.component.ts
+++ src/app/notification-form/notification-option/notification-option.component.ts
@@ -13,6 +13,7 @@ export class NotificationOptionComponent implements OnInit {
   @Input() form: FormGroup;
   @Input() control: string;
   @Input() tooltip: string = undefined;
+  @Input() position: string | null = null;
   @Input() disabled: boolean;
 
   constructor() { }
