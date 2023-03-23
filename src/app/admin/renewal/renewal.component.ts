5abfe95ca6c3c660ca2986beb9936d6e10d50bd5
paul2260710 <paul@smartpass.app>
2023-03-23 16:06:13 -0400

    Dropdown text for selected features

--- src/app/admin/renewal/renewal.component.ts
+++ src/app/admin/renewal/renewal.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-renewal',
-  templateUrl: './renewal.component.html',
-  styleUrls: ['./renewal.component.scss']
+	selector: 'app-renewal',
+	templateUrl: './renewal.component.html',
+	styleUrls: ['./renewal.component.scss'],
 })
 export class RenewalComponent implements OnInit {
+	public selectedFeature = 0;
 
-  constructor() { }
+	ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+	handleFeatureClick(clicked: number) {
+		if (this.selectedFeature === clicked) {
+			this.selectedFeature = 0;
+		} else {
+			this.selectedFeature = clicked;
+		}
+	}
 }
