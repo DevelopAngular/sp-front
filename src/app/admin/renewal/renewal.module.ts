8e2c7ee6defb189236dfe65dabb915b8f0a89f23
paul2260710 <paul@smartpass.app>
2023-03-23 13:40:36 -0400

    Countdown clock added

--- src/app/admin/renewal/renewal.module.ts
+++ src/app/admin/renewal/renewal.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RenewalComponent } from './renewal.component';
 import { RenewalRoutingModule } from './renewal-routing.module';
+import { CountdownComponent } from './countdown/countdown.component';
 
 @NgModule({
-	declarations: [RenewalComponent],
+	declarations: [RenewalComponent, CountdownComponent],
 	imports: [CommonModule, RenewalRoutingModule],
 })
 export class RenewalModule {}
