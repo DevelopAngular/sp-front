b92551784f05c35ba2151d768ffb2b1bfaa7a55b
Kane Gui <kane@smartpass.app>
2023-05-09 16:32:26 -0400

    applied fixes

--- src/app/services/teacher-reviews.service.ts
+++ src/app/services/teacher-reviews.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { UserService } from './user.service';
 import { HttpClient } from '@angular/common/http';
 import { HttpService } from './http-service';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, forkJoin } from 'rxjs';
+import { map, switchMap} from 'rxjs/operators';
 
 interface TeacherReview {
 	name: string;
@@ -19,45 +19,56 @@ interface TeacherReview {
 export class TeacherReviewsService {
 	constructor(private userService: UserService, private http: HttpClient, private httpService: HttpService) {}
 
-	getReviews(): Observable<TeacherReview[]> {
-		return this.http.get('../assets/xlsx/Teacher Survey.csv', { responseType: 'text' }).pipe(
-			map((csvRawText) => {
-				const lines = csvRawText.split(/\n/);
-				let reviews: TeacherReview[] = [];
 
-				for (let i = 1; i < lines.length; i++) {
-					const regex = /,(?=(?:[^"]*"[^"]*")*[^"]*$)/; //commas outside of quotes
-					const fields = lines[i].split(regex);
-					//const response_id = fields[0];
-					const what_to_display = fields[1];
-					const school_id = fields[2];
-					//const email = fields[3];
-					const do_not_share = fields[4];
-					const user_id = fields[5];
-					const stars = parseInt(fields[6]);
-					const testimonial = this.formatTestimonial(fields[7]); //strip first 2 and last 2 double quotes because of .csv formatting after converting from .numbers
-					const first_shown = this.formatDate(fields[8]);
-
-					if (this.httpService.getSchool().id == school_id && !do_not_share) {
-						this.userService.getUserById(user_id).subscribe((user) => {
-							if (user && user.roles.includes('_profile_teacher')) {
-								const review: TeacherReview = {
-									name: user.display_name,
-									what_to_display: what_to_display,
-									stars: stars,
-									testimonial: testimonial,
-									first_shown: first_shown,
-								};
-
-								reviews.push(review);
-							}
-						});
-					}
-				}
-				return reviews;
-			})
-		);
-	}
+  getReviews(): Observable<TeacherReview[]> {
+    return this.http.get('../assets/xlsx/Teacher Survey.csv', { responseType: 'text' }).pipe(
+      switchMap((csvRawText) => {
+        const lines = csvRawText.split(/\n/);
+        let reviewObservables: Observable<TeacherReview>[] = [];
+  
+        for (let i = 1; i < lines.length; i++) {
+          const regex = /,(?=(?:[^"]*"[^"]*")*[^"]*$)/; //commas outside of quotes
+          const fields = lines[i].split(regex);
+          const what_to_display = fields[1];
+          const school_id = fields[2];
+          const do_not_share = fields[4];
+          const user_id = fields[5];
+          const stars = parseInt(fields[6]);
+          const testimonial = this.formatTestimonial(fields[7]);
+          const first_shown = this.formatDate(fields[8]);
+  
+          if (this.httpService.getSchool().id == school_id && !do_not_share) {
+            const userObs = this.userService.getUserById(user_id).pipe(
+              map((user) => {
+                if (user && user.roles.includes('_profile_teacher')) {
+                  const review: TeacherReview = {
+                    name: user.display_name,
+                    what_to_display: what_to_display,
+                    stars: stars,
+                    testimonial: testimonial,
+                    first_shown: first_shown,
+                  };
+  
+                  return review;
+                } else {
+                  return null;
+                }
+              })
+            );
+  
+            reviewObservables.push(userObs);
+          }
+        }
+  
+        return forkJoin(reviewObservables).pipe(
+          map((reviewResults) => {
+            // Filter out null results and return the populated reviews array
+            return reviewResults.filter((review) => review !== null);
+          })
+        );
+      })
+    );
+  }
 
 	//csv has weird formatting where it added 2 sets of double quotes to all strings
 	formatTestimonial(str: string): string {
