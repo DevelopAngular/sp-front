3e2cff1df3cf02fe840ceff3a199bca2a01494f9
Paul Lantow <paul@smartpass.app>
2023-05-20 00:33:31 +0000

    Referral Tweaking

--- src/app/referrals/referral-page/referral-page.component.ts
+++ src/app/referrals/referral-page/referral-page.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { User } from '../../models/User';
+import { concatMap } from 'rxjs/operators';
+import { DarkThemeSwitch } from '../../dark-theme-switch';
+
 @Component({
 	selector: 'sp-referral-page',
 	templateUrl: './referral-page.component.html',
 	styleUrls: ['./referral-page.component.scss'],
 })
 export class ReferralPageComponent implements OnInit {
-	constructor() {}
+	user: User;
+	constructor(private userService: UserService, public darkTheme: DarkThemeSwitch) {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.userService.userData.pipe(concatMap((user) => this.userService.getUserById(user.id))).subscribe((user) => {
+			this.user = User.fromJSON(user);
+		});
+	}
 }
